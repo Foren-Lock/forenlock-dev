7003ac398f49580721d142da38c792817f857bc2
Foren-Lock <finalresearch11@gmail.com>
2025-05-21 13:35:08 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const isDev = mode === "development";
-
+export default defineConfig(() => {
   return {
     server: {
-      host: "localhost", // more common than "::" (IPv6)
+      host: "localhost",
       port: 8080,
     },
     plugins: [
       react(),
-      ...(isDev ? [componentTagger()] : []),
     ],
     resolve: {
       alias: {
